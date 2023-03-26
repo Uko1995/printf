0cc5b955965fe75bf28ec2e4136712d87892f617
Uko Uwatt <uwattuko@gmail.com>
2023-03-26 13:56:40 -0700

    update main.h

--- main.h
+++ main.h
@@ -1,8 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include<stdarg.h>
+#include<string.h>
+#include<unistd.h>
+
 int _printf(const char *format, ...);
+int _printf_int(const char *format, ...);
 
 
+/*functions.c*/
+char _printchar(char c, va_list args);
+char _printchar(char *s, va_list args);
+int _printchar(int d, va_list args);
+int _printchar(char i, va_list args);
 
 
 #endif
