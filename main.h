159212092e4f72fd7200e6920b77df8b1d48cc67
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-03-26 15:42:01 -0700

    updated headerfile

--- main.h
+++ main.h
@@ -10,9 +10,10 @@ int _printf_int(const char *format, ...);
 
 /*functions.c*/
 char _printchar(char c, va_list args);
-char _printchar(char *s, va_list args);
-int _printchar(int d, va_list args);
-int _printchar(char i, va_list args);
+char _printstring(char *s, va_list args);
+int _printd(int d, va_list args);
+int _printi(int i, va_list args);
+void (*format)(const char *format);
 
 
 #endif
