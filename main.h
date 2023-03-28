69f40c1a1abc22b178b27bdda36176f1827fb5c1
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-03-28 09:42:47 -0700

    made corrections to header file

--- main.h
+++ main.h
@@ -5,14 +5,25 @@
 #include<unistd.h>
 
 int _printf(const char *format, ...);
-int _printf_int(const char *format, ...);
+//int _printf_int(const char *format, ...);
 
 
 /*functions.c*/
-char _printchar(char c, va_list args);
-char *_printstring(char *s, va_list args);
-int _printd(int d, va_list args);
-int _printi(int i, va_list args);
+//char _printchar(char c, va_list args);
+//char *_printstring(char *s, va_list args);
+//int _printd(int d, va_list args);
+//int _printi(int i, va_list args);
+typedef struct function
+{
+	char *t;
+	int(*f)(va_list args);
+}point;
+int print_char(va_list args);
+int print_string(va_list args);
+int print_cent(va_list args);
+int printi(va_list args);
+int printd(va_list args);
+int (*specifier(const char *format))(va_list);
 
 
 
